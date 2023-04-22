66bc235f5d0459a082597bad955b335d92a02a5b
Eduardo Koloma Junior <eduardo@kolomajr.com>
2023-04-22 19:15:53 +0200

    ✨ add nats projection config

---
+++ projection/nats/config.go
@@ -0,0 +1,10 @@
+package nats
+
+type ProjectionConfig struct {
+	Name       string `json:"name,omitempty" mapstructure:"name,omitempty"`
+	Enabled    bool   `json:"enabled,omitempty" mapstructure:"enabled,omitempty"`
+	Durable    bool   `json:"durable,omitempty" mapstructure:"durable,omitempty"`
+	QueueGroup string `json:"queue_group,omitempty" mapstructure:"queue_group,omitempty"`
+	KVName     string `json:"kv_name,omitempty" mapstructure:"kv_name,omitempty"`
+	NoKV       bool   `json:"no_kv,omitempty" mapstructure:"no_kv,omitempty"`
+}
