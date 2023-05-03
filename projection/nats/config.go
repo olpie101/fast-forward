1bf08657a6de6ff05288de2dd98b2f89cf7bd754
Eduardo Koloma Junior <eduardo@kolomajr.com>
2023-05-03 22:37:44 +0200

    ✨ allow multiple kv names for projection config

--- projection/nats/config.go
+++ projection/nats/config.go
@@ -1,10 +1,11 @@
 package nats
 
 type ProjectionConfig struct {
-	Name       string `json:"name,omitempty" mapstructure:"name,omitempty"`
-	Enabled    bool   `json:"enabled,omitempty" mapstructure:"enabled,omitempty"`
-	Durable    bool   `json:"durable,omitempty" mapstructure:"durable,omitempty"`
-	QueueGroup string `json:"queue_group,omitempty" mapstructure:"queue_group,omitempty"`
-	KVName     string `json:"kv_name,omitempty" mapstructure:"kv_name,omitempty"`
-	NoKV       bool   `json:"no_kv,omitempty" mapstructure:"no_kv,omitempty"`
+	Name       string            `json:"name,omitempty" mapstructure:"name,omitempty"`
+	Enabled    bool              `json:"enabled,omitempty" mapstructure:"enabled,omitempty"`
+	Durable    bool              `json:"durable,omitempty" mapstructure:"durable,omitempty"`
+	QueueGroup string            `json:"queue_group,omitempty" mapstructure:"queue_group,omitempty"`
+	KVName     string            `json:"kv_name,omitempty" mapstructure:"kv_name,omitempty"`
+	KVNames    map[string]string `json:"kv_names,omitempty" mapstructure:"kv_names,omitempty"`
+	NoKV       bool              `json:"no_kv,omitempty" mapstructure:"no_kv,omitempty"`
 }
