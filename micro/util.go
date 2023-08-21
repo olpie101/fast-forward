47031950f60adc78361f12aaad8bc2051c99b572
Eduardo Koloma Junior <eduardo@kolomajr.com>
2023-08-21 13:15:05 +0200

    ✨ add withToken context updater

--- micro/util.go
+++ micro/util.go
@@ -2,16 +2,20 @@ package micro
 
 import (
 	"context"
+	"strings"
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/nats-io/jwt/v2"
 	"github.com/nats-io/nats.go/micro"
 )
 
 type ContextKey string
 
 var (
-	ContextKeyUserId = ContextKey("userId")
+	ContextKeyUserId   = ContextKey("userId")
+	ContextKeyUserName = ContextKey("name")
+	ContextKeyUserOrg  = ContextKey("org")
 )
 
 type ErrorableMicroRequest func(ctx context.Context, r micro.Request) error
@@ -40,6 +44,33 @@ func WithUserID(h func(ctx context.Context, r micro.Request)) func(ctx context.C
 		id, _ := uuid.Parse(uid)
 		ctx = context.WithValue(ctx, ContextKeyUserId, id)
 		h(ctx, r)
+	}
+}
+
+func WithToken(h ErrorableMicroRequest) ErrorableMicroRequest {
+	return func(ctx context.Context, r micro.Request) error {
+		token := r.Headers().Get("token")
+		claims, err := jwt.DecodeUserClaims(token)
+		if err != nil {
+			return err
+		}
+		uid, err := uuid.Parse(claims.Name)
+		if err != nil {
+			return err
+		}
+		ctx = context.WithValue(ctx, ContextKeyUserId, uid)
+		for _, tag := range claims.Tags {
+			if strings.HasPrefix(tag, "name") {
+				parts := strings.Split(tag, ":")
+				ctx = context.WithValue(ctx, ContextKeyUserName, parts[1])
+			}
+
+			if strings.HasPrefix(tag, "org") {
+				parts := strings.Split(tag, ":")
+				ctx = context.WithValue(ctx, ContextKeyUserOrg, parts[1])
+			}
+		}
+		return h(ctx, r)
 	}
 }
 
