de10f71c2112bf7a8c3be26cbb27a551dde519e6
Eduardo Koloma Junior <eduardo@kolomajr.com>
2024-08-14 23:57:34 +0200

    🐛 fetch by version time if single aggregate and min version set

--- store/nats/query_limit_guard.go
+++ store/nats/query_limit_guard.go
@@ -1,6 +1,8 @@
 package nats
 
 import (
+	"time"
+
 	"github.com/modernice/goes/event"
 	"github.com/modernice/goes/helper/pick"
 	"golang.org/x/exp/slices"
@@ -11,6 +13,14 @@ type limitGuard struct {
 	maxVersionGuard func(e event.Event) bool
 	minTimeGuard    func(e event.Event) bool
 	maxTimeGuard    func(e event.Event) bool
+	hasMinVersion   bool
+	minVersion      int
+	hasMaxVersion   bool
+	maxVersion      int
+	hasMinTime      bool
+	minTime         time.Time
+	hasMaxTime      bool
+	maxTime         time.Time
 }
 
 func (g limitGuard) guard(e event.Event) bool {
@@ -31,12 +41,16 @@ func newLimitGuard(q event.Query) limitGuard {
 			guard.minVersionGuard = func(e event.Event) bool {
 				return pick.AggregateVersion(e) >= min
 			}
+			guard.hasMinVersion = true
+			guard.minVersion = min
 		}
 		if len(q.AggregateVersions().Max()) > 0 {
 			max := slices.Min(q.AggregateVersions().Max())
 			guard.maxVersionGuard = func(e event.Event) bool {
 				return pick.AggregateVersion(e) <= max
 			}
+			guard.hasMaxVersion = true
+			guard.maxVersion = max
 		}
 	}
 
@@ -45,11 +59,15 @@ func newLimitGuard(q event.Query) limitGuard {
 			guard.minTimeGuard = func(e event.Event) bool {
 				return e.Time().After(q.Times().Min())
 			}
+			guard.hasMinTime = true
+			guard.minTime = q.Times().Min()
 		}
 		if !q.Times().Max().IsZero() {
 			guard.maxTimeGuard = func(e event.Event) bool {
 				return e.Time().Before(q.Times().Max())
 			}
+			guard.hasMaxTime = true
+			guard.maxTime = q.Times().Max()
 		}
 	}
 
