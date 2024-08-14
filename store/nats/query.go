bb64e9f91f431fdaa79b73d146d0c6e58fc2d3db
Eduardo Koloma Jr <5549816+olpie101@users.noreply.github.com>
2024-08-14 23:58:18 +0200

    🐛 fetch by version time if single aggregate and min version set (#19)

--- store/nats/query.go
+++ store/nats/query.go
@@ -2,6 +2,7 @@ package nats
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"strings"
 	"sync"
@@ -52,9 +53,23 @@ func (s *Store) query(ctx context.Context, q event.Query, subjects []string) (<-
 	cmsgs := make(chan jetstream.Msg, 200)
 	push := streams.ConcurrentContext(subCtx, cmsgs)
 	subErrs := make(chan error, 1)
+	minTime := q.Times().Min()
+
+	if len(subjects) == 1 && len(q.AggregateIDs()) == 1 && guard.hasMinVersion && guard.minVersion > 1 {
+		st := time.Now()
+		for stream, subjects := range groups {
+			t, err := s.fetchVersionMinTime(ctx, stream, subjects[0], guard.minVersion-1)
+			if err != nil {
+				s.logger.Errorw("unable to get aggregate min time", "subject", subjects[0], "error", err)
+				break
+			}
+			minTime = t
+		}
+		s.logger.Infow("aggreagte fetch version metadata", "d", time.Since(st))
+	}
 
 	for stream, subjects := range groups {
-		go s.subFn(subCtx, &wg, stream, subjects, q.Times().Min(), push, subErrs)
+		go s.subFn(subCtx, &wg, stream, subjects, minTime, push, subErrs)
 	}
 
 	go func() {
@@ -168,6 +183,35 @@ func (s *Store) identifyStreams(ctx context.Context, q event.Query) error {
 	}
 
 	return nil
+}
+
+func (s *Store) fetchVersionMinTime(ctx context.Context, stream, subject string, version int) (time.Time, error) {
+	parts := strings.Split(subject, ".")
+	if len(parts) != 5 && parts[3] != "*" {
+		return time.Time{}, errors.New("unknown subject format to identify version")
+	}
+
+	subject = strings.Replace(subject, "*", fmt.Sprintf("%d", version), 1)
+	s.logger.Debugw("fetching aggregate version meta", "subject", subject)
+	c, err := s.js.CreateOrUpdateConsumer(ctx, stream, jetstream.ConsumerConfig{
+		DeliverPolicy: jetstream.DeliverAllPolicy,
+		AckPolicy:     jetstream.AckNonePolicy,
+		FilterSubject: subject,
+		HeadersOnly:   true,
+		Replicas:      1,
+		MemoryStorage: true,
+	})
+
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	msg, err := c.Next(jetstream.FetchMaxWait(10 * time.Second))
+	if err != nil {
+		return time.Time{}, err
+	}
+	_, _, t, err := parseEventValues(msg.Headers())
+	return t, err
 }
 
 func (s *Store) subscribe(ctx context.Context, wg *sync.WaitGroup, stream string, subjects []string, startTime time.Time, push func(...jetstream.Msg) error, errs chan<- error) {
