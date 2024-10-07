0f240137244250707f7289193f5479e7fb47baea
Tes-sudo <penieltesfaye100@gmail.com>
2024-10-07 15:32:10 +0200

    Template and generator

--- cmd/gen/main.go
+++ cmd/gen/main.go
@@ -23,20 +23,22 @@ import (
 
 func main() {
 	serviceName := flag.String("service-name", "CardService", "service name to generate schema for")
-	// templatePath := flag.String("template", "", "template file")
+	templatePath := flag.String("template", "", "template file")
 	outPath := flag.String("output", "card.gen.ts", "output file location")
 	importDefinitionsPath := flag.String("import-definitions-path", "", "import go definitions from")
 	language := flag.String("lang", "ts", "language to generate")
 	casing := flag.String("casing", "snake", "method casing")
 	flag.Parse()
 
-	// fmt.Println(*templatePath)
-	// var outPath string
-	// serviceName = "AuthService"
-	templatePath := "./templates/ts.client.tmpl"
-	templateName := filepath.Base(templatePath)
+	templateName := filepath.Base(*templatePath)
 
-	err := execute(*serviceName, templatePath, templateName, *importDefinitionsPath, *language, *casing, *outPath)
+	var err error
+	if *language == "python" {
+		err = executePython(*serviceName, *templatePath, templateName, *importDefinitionsPath, *language, *casing, *outPath)
+	} else {
+		err = execute(*serviceName, *templatePath, templateName, *importDefinitionsPath, *language, *casing, *outPath)
+	}
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -48,7 +50,6 @@ func execute(name string, templatePath, templateName, importDefs, lang, casing, 
 	nc, err := nats.Connect(
 		os.Getenv("NATS_URL"),
 		nats.UserCredentials(os.Getenv("NATS_CREDS")),
-		// nats.UserCredentials("/Users/eduardokolomajr/.local/share/nats/nsc/keys/creds/sava_technologies/NEXUS_STAGING_KE/backend-service.creds"),
 	)
 	if err != nil {
 		return err
@@ -75,11 +76,10 @@ func execute(name string, templatePath, templateName, importDefs, lang, casing, 
 	}
 
 	data := TemplateData{
-		Name: resp.Name,
-		// APIURL:    resp.APIURL,
-		Endpoints:     []EndpointConfig{},
-		ImportPackage: importDefs,
-		PackagePrefix: importPackage,
+		Name:           resp.Name,
+		Endpoints:      []EndpointConfig{},
+		ImportPackage:  importDefs,
+		PackagePrefix:  importPackage,
 	}
 	fmt.Println("ID:", resp.ID, resp.ServiceIdentity)
 
@@ -154,6 +154,102 @@ func execute(name string, templatePath, templateName, importDefs, lang, casing, 
 	return os.WriteFile(outPath, w.Bytes(), 0644)
 }
 
+func executePython(name string, templatePath, templateName, importDefs, lang, casing, outPath string) error {
+	nc, err := nats.Connect(
+		os.Getenv("NATS_URL"),
+		nats.UserCredentials(os.Getenv("NATS_CREDS")),
+	)
+	if err != nil {
+		return err
+	}
+	sub, err := micro.ControlSubject(micro.InfoVerb, name, "")
+	if err != nil {
+		return err
+	}
+	m, err := nc.Request(sub, nil, time.Second)
+	if err != nil {
+		return err
+	}
+	var resp micro.Info
+	err = json.Unmarshal(m.Data, &resp)
+	if err != nil {
+		return err
+	}
+
+	packageParts := strings.Split(importDefs, "/")
+	importPackage := ""
+	if len(packageParts) > 0 {
+		importPackage = packageParts[len(packageParts)-1]
+	}
+
+	data := TemplateData{
+		Name:           resp.Name,
+		Endpoints:      []EndpointConfig{},
+		ImportPackage:  importDefs,
+		PackagePrefix:  importPackage,
+	}
+
+	wildcardCountSet := false
+
+	t, err := template.New("service").ParseFiles(templatePath)
+	if err != nil {
+		return err
+	}
+
+	for _, ep := range resp.Endpoints {
+		var cfg EndpointConfig
+		cfg.Name = convertToCase(ep.Name, casing)
+		cfg.Subject = ep.Subject
+		wc := strings.Count(ep.Subject, "*")
+		if !wildcardCountSet && wc > 0 {
+			wildcardCountSet = true
+			data.SubjectParamsCount = wc
+		}
+		if wildcardCountSet && wc != data.SubjectParamsCount {
+			return errors.New("cannot generate client for endpoints with varying wildcard counts")
+		}
+
+		var schema ffmicro.Schema
+		err := json.Unmarshal([]byte(ep.Metadata["schema"]), &schema)
+		if err != nil {
+			return err
+		}
+
+		reqSchema, err := unmarshalSchema(schema.Request)
+		if err != nil {
+			return err
+		}
+
+		respSchema, err := unmarshalSchema(schema.Response)
+		if err != nil {
+			return err
+		}
+
+		cfg.Schema.Request = extractName(reqSchema)
+		cfg.Schema.Response = extractName(respSchema)
+
+		if lang == "python" {
+			cfg.Schema.RequestType = generatePythonTypeDefinition(cfg.Schema.Request, reqSchema)
+			cfg.Schema.ResponseType = generatePythonTypeDefinition(cfg.Schema.Response, respSchema)
+		}
+
+		data.Endpoints = append(data.Endpoints, cfg)
+	}
+
+	if data.SubjectParamsCount > 0 {
+		def := strings.TrimSuffix(strings.Repeat("str,", data.SubjectParamsCount), ",")
+		data.SubjectParams = fmt.Sprintf("List[%s]", def)
+	}
+
+	var w bytes.Buffer
+	err = t.ExecuteTemplate(&w, templateName, data)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(outPath, w.Bytes(), 0644)
+}
+
 func convertToCase(s, casing string) string {
 	var out stringy.StringManipulation
 	switch casing {
@@ -164,18 +260,16 @@ func convertToCase(s, casing string) string {
 	default:
 		out = stringy.New(s).SnakeCase()
 	}
-
 	return out.Get()
 }
 
 type TemplateData struct {
-	Name string `json:"name,omitempty"`
-	// APIURL             string           `json:"api_url,omitempty"`
-	SubjectParams      string `json:"subject_params,omitempty"`
-	SubjectParamsCount int    `json:"subject_params_count,omitempty"`
+	Name               string
+	SubjectParams      string
+	SubjectParamsCount int
 	ImportPackage      string
 	PackagePrefix      string
-	Endpoints          []EndpointConfig `json:"endpoints,omitempty"`
+	Endpoints          []EndpointConfig
 }
 
 type EndpointConfig struct {
@@ -187,8 +281,10 @@ type EndpointConfig struct {
 }
 
 type RequestResponseSchema struct {
-	Request  string
-	Response string
+	Request      string
+	Response     string
+	RequestType  string
+	ResponseType string
 }
 
 func unmarshalSchema(def string) (*jsonschema.Schema, error) {
@@ -230,4 +326,49 @@ func generateReqRespInterfaces(schemas ...*jsonschema.Schema) ([]byte, error) {
 		}
 	}
 	return tw.Bytes(), nil
+}
+
+func generatePythonTypeDefinition(name string, schema *jsonschema.Schema) string {
+	var def strings.Builder
+	def.WriteString(fmt.Sprintf("class %s(TypedDict):\n", name))
+	
+	if schema.Properties != nil {
+		for pair := schema.Properties.Oldest(); pair != nil; pair = pair.Next() {
+			key := pair.Key
+			value := pair.Value
+			typeName := getPythonType(value)
+			def.WriteString(fmt.Sprintf("    %s: %s\n", key, typeName))
+		}
+	}
+	
+	if schema.Properties == nil || schema.Properties.Len() == 0 {
+		def.WriteString("    pass\n")
+	}
+	
+	def.WriteString("\n")
+	
+	return def.String()
+}
+
+func getPythonType(prop *jsonschema.Schema) string {
+	switch prop.Type {
+	case "string":
+		return "str"
+	case "integer":
+		return "int"
+	case "number":
+		return "float"
+	case "boolean":
+		return "bool"
+	case "array":
+		if prop.Items != nil {
+			itemType := getPythonType(prop.Items)
+			return fmt.Sprintf("List[%s]", itemType)
+		}
+		return "List[Any]"
+	case "object":
+		return "Dict[str, Any]"
+	default:
+		return "Any"
+	}
 }
