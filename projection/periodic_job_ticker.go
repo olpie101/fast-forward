9d200106e8f9eed64ecd6ad9c00ab00b71e6df17
Eduardo Koloma Jr <5549816+olpie101@users.noreply.github.com>
2024-11-08 00:22:17 +0100

    ✨ implement job ticker with query func (#21)

--- projection/periodic_job_ticker.go
+++ projection/periodic_job_ticker.go
@@ -2,6 +2,7 @@ package projection
 
 import (
 	"context"
+	"fmt"
 	"time"
 
 	"github.com/modernice/goes/event/query"
@@ -29,6 +30,50 @@ func PeriodicJobTicker(ctx context.Context, d time.Duration, s projection.Schedu
 					projection.Query(
 						query.New(
 							mergedOpts...,
+						),
+					),
+				)
+				if err != nil {
+					errs <- err
+				}
+			}
+
+		}
+	}()
+
+	return errs
+}
+
+func PeriodicJobTickerFunc(ctx context.Context, d time.Duration, s projection.Schedule, tickFn func(context.Context) ([]query.Option, error), opts ...query.Option) <-chan error {
+	t := time.NewTicker(d)
+
+	mergedOpts := []query.Option{
+		query.SortByTime(),
+	}
+
+	mergedOpts = append(mergedOpts, opts...)
+	errs := make(chan error)
+	go func() {
+		for {
+			select {
+			case <-ctx.Done():
+				t.Stop()
+				return
+			case <-t.C:
+				extraOpts, err := tickFn(ctx)
+
+				if err != nil {
+					errs <- fmt.Errorf("job tick func error: %w", err)
+					continue
+				}
+
+				allOpts := append(mergedOpts, extraOpts...)
+
+				err = s.Trigger(
+					ctx,
+					projection.Query(
+						query.New(
+							allOpts...,
 						),
 					),
 				)
